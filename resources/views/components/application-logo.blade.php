5c21b05e8dd3d992b4100f0a3f5b5bfda9dc6546
unknown <kingikhamis518@gmail.com>
2025-03-25 15:33:06 +0300

    svg

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,3 @@
-<xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <img src="{{ asset('images/logo.png') }}" alt="Logo" width="50" height="50">
 <style type="text/css">
